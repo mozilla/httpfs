8a5445444a2d741ab81ff7d40176184121d6cdee
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2018-09-21 19:01:35 +0200

    WIP

--- httpfs.js
+++ httpfs.js
@@ -4,9 +4,10 @@ const url = require('url')
 const program = require('commander')
 const fuse = require('fuse-bindings')
 const Agent = require('agentkeepalive')
+const filesizeParser = require('filesize-parser')
 const BufferSerializer = require('buffer-serializer')
 
-const operations = 'getattr readdir readlink truncate chown chmod read write create utimens unlink rename link symlink mkdir rmdir'.split(' ')
+const operations = 'options getattr readdir readlink truncate chown chmod read write create utimens unlink rename link symlink mkdir rmdir'.split(' ')
 
 var serializer = new BufferSerializer()
 var serviceUrl
@@ -15,17 +16,24 @@ var quiet
 var certificate
 var http
 var agent
-var timeout = 60 * 60
-var callcounter = 0
+var timeout
 var calls = []
 var running = true
+var attrcache = true
+var blocksize = 0
+var nocache
+var cache = {}
+var cacheTop = []
 
 program
     .version('0.0.1')
     .option('-q, --quiet')
     .option('-t, --timeout <seconds>')
-    .option('-cr, --certraw <certificate>')
-    .option('-cf, --certfile <certificate-filename>')
+    .option('--attrcache')
+    .option('--blocksize <bytes>')
+    .option('--nocache <regex>')
+    .option('--certraw <certificate>')
+    .option('--certfile <certificate-filename>')
     .arguments('<url> <mountpath>')
     .action((surl, mountpath, options) => {
         serviceUrl = url.parse(surl)
@@ -35,10 +43,13 @@ program
         } else if (options.certfile) {
             certificate = fs.readFileSync(options.certfile)
         }
-        if (options.timeout) {
-            timeout = options.timeout
+        if (options.nocache) {
+            nocache = new RegExp(options.nocache)
         }
+        blocksize = options.blocksize ? filesizeParser(options.blocksize) : 0
+        timeout = options.timeout || 60 * 60
         quiet = !!options.quiet
+        attrcache = !!options.attrcache
     })
     .parse(process.argv)
 
@@ -67,15 +78,103 @@ function removeCall(call) {
     }
 }
 
+function finishCall(call, ...args) {
+    removeCall(call)
+    call.callback(...args)
+}
+
+function flushFile(filepath, cb) {
+    let cached = cache[filepath]
+    if (cached && cached.block && cached.block.length > 0) {
+        let b = Buffer.concat(cached.block)
+        perform('write', filepath, 0, b, b.length, cached.position, cb)
+        cached.block = []
+        cached.position += b.length
+    }
+}
+
 function sendRequest(call, retries) {
     let args = call.args
-    let rargs
+    let rargs = args
     switch (call.operation) {
-        case 'read':        rargs = [args[0], args[1], args[3], args[4]]; break
-        case 'write':       rargs = [args[0], args[1], args[2].slice(0, args[3]), args[4]]; break
+        case 'getattr':     
+            if (attrcache && cached && cached.stat) {
+                return finishCall(0, cached.stat)
+            }
+            break
+        case 'read':  
+            [filepath, fd, buffer, length, position] = args   
+            if (blocksize > 0 && !buffer.isBuffer && (!nocache || !nocache.test(filepath))) {
+                let cached = cache[filepath]
+                if (cached && 
+                    !cached.out &&
+                    cached.block && 
+                    cached.position <= position && 
+                    cached.position + cached.block.length >= position + length) {
+                    let sourceStart = position - cached.position
+                    cached.block.copy(buffer, 0, sourceStart, sourceStart + length)
+                    finishCall(call, 0, length)
+                } else if (cached && cached.out && cached.block) {
+                    flushFile(filepath, () => sendRequest(call, retries))
+                } else if (cached && !cached.out && !cached.block) {
+                    call.timer = setTimeout(() => sendRequest(call, retries - 1), 1000)
+                } else {
+                    cached = cache[filepath] = cached || {}
+                    cached.position = position
+                    cached.out = false
+                    cached.block = null
+                    let index = cacheTop.indexOf(cached)
+                    if (index >= 0) {
+                        cacheTop.splice(index)
+                    }
+                    cacheTop.push(cached)
+                    if (cacheTop.length > 10) {
+                        let oldest = cacheTop.shift()
+                        if (oldest.out && oldest.block) {
+                            flushFile(filepath, () => {})
+                        }
+                    }
+                    perform('read', filepath, 0, cached, Math.max(length, blocksize), position, l => {
+                        if (l < 0) {
+                            finishCall(call, l)
+                        } else if (l < length) {
+                            let actualLength = Math.min(l, length)
+                            cached.block.copy(buffer, 0, 0, actualLength)
+                            finishCall(call, actualLength)
+                        } else {
+                            sendRequest(call, retries)
+                        }
+                    })
+                }
+                return
+            }
+            rargs = [filepath, fd, length, position]
+            break
+        case 'write':
+            [filepath, fd, buffer, length, position] = args  
+            buffer = buffer.length == length ? buffer : buffer.slice(0, length) 
+            if (blocksize > 0 && (!nocache || !nocache.test(filepath))) {
+                cached = cache[filepath] = cached || {}
+                if (cached.out) {
+                    let blockLength = cached.block.reduce((l, b) => l + b, 0)
+                    let cursor = cached.position + blockLength
+                    if (position == cursor || cursor == 0) {
+                        cached.block.push(buffer)
+                        cached.position = position
+                    } else {
+                        flushFile(filepath, () => sendRequest(call, retries))
+                    }
+                } else {
+                    cached.out = true
+                    cached.block = [buffer]
+                    cached.position = position
+                }
+                return
+            }
+            rargs = [filepath, fd, buffer, position]
+            break
         case 'link':        rargs = [args[1], args[0]]; break
-        case 'symlink':     rargs = [args[1], args[0]]; break
-        default:            rargs = args
+        case 'symlink':     rargs = [args[1], args[0]]; break  
     }
     let buffer = serializer.toBuffer({ operation: call.operation, args: rargs })
     options = {
@@ -101,7 +200,11 @@ function sendRequest(call, retries) {
             switch (call.operation) {
                 case 'read':
                     if (result[0] >= 0) {
-                        result[1].copy(args[2])
+                        if (args[2].isBuffer) {
+                            result[1].copy(args[2])
+                        } else {
+                            args[2].block = result[1]
+                        }
                         result = [result[1].length]
                     }
                     break
@@ -124,10 +227,11 @@ function sendRequest(call, retries) {
     call.request.end(buffer)
 }
 
-function perform(operation, args) {
-    let callback = args.pop()
-    if (running) {
-        let call = { id: callcounter++, operation: operation, args: args, callback: callback }
+function perform(operation, ...args) {
+    if (operation == 'options') {
+        return ['nonempty']
+    } else if (running) {
+        let call = { operation: operation, args: args, callback: args.pop() }
         calls.push(call)
         sendRequest(call, timeout)
     } else {
@@ -137,8 +241,8 @@ function perform(operation, args) {
 
 fuse.mount(mountPath, new Proxy({}, {
     ownKeys: (target, key) => operations,
-    getOwnPropertyDescriptor: (target, key) => operations.includes(key) ? { value: (...args) => perform(key, args), enumerable: true, configurable: true } : undefined,
-    get: (target, key) => (...args) => perform(key, args)
+    getOwnPropertyDescriptor: (target, key) => operations.includes(key) ? { value: (...args) => perform(key, ...args), enumerable: true, configurable: true } : undefined,
+    get: (target, key) => (...args) => perform(key, ...args)
 }), function (err) {
     if (err) throw err
     log('filesystem mounted on ' + mountPath)
