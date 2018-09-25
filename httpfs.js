479fc929ad43481b48851ea5b762b7bcb38ef0f5
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2018-09-25 20:13:21 +0200

    Caching

--- httpfs.js
+++ httpfs.js
@@ -7,10 +7,8 @@ const Agent = require('agentkeepalive')
 const filesizeParser = require('filesize-parser')
 const BufferSerializer = require('buffer-serializer')
 
-const operations = 'options getattr readdir readlink truncate chown chmod read write create utimens unlink rename link symlink mkdir rmdir'.split(' ')
-
 var serializer = new BufferSerializer()
-var serviceUrl
+var serviceUrl = url.parse('http://localhost:3000')
 var mountPath
 var quiet
 var certificate
@@ -20,8 +18,7 @@ var timeout
 var calls = []
 var running = true
 var attrcache
-var readcache
-var writecache
+var cache
 var blocksize = 0
 var nocache
 
@@ -29,8 +26,7 @@ program
     .version('0.0.1')
     .option('-q, --quiet')
     .option('-t, --timeout <seconds>')
-    .option('--attrcache')
-    .option('--readcache')
+    .option('--cache')
     .option('--writecache')
     .option('--blocksize <bytes>')
     .option('--nocache <regex>')
@@ -49,13 +45,10 @@ program
             nocache = new RegExp(options.nocache)
         }
         if (options.attrcache) {
-            attrcache = []
+            attrcache = {}
         }
-        if (options.readcache) {
-            readcache = []
-        }
-        if (options.writecache) {
-            writecache = []
+        if (options.cache) {
+            cache = []
         }
         blocksize = options.blocksize ? filesizeParser(options.blocksize) : 1024 * 1024
         timeout = options.timeout || 60 * 60
@@ -157,37 +150,159 @@ function setAttrCache(p, stat) {
     }
 }
 
+function shouldCache(p) {
+    return cache && !(nocache && nocache.test(p))
+}
+
+function createDescriptor(p) {
+    return shouldCache(p) ? createCache(p) : 0
+}
+
+function isCached(fd) {
+    return fd > 0
+}
+
+function createCache(p) {
+    for(let i = 1; i <= cache.length + 1; i++) {
+        if (!cache[i]) {
+            cache[i] = {
+                p: p,
+                write: {
+                    off: 0,
+                    pos: 0,
+                    blocks: []
+                },
+                read: {
+                    off: 0,
+                    block: null
+                }
+            }
+            return i
+        }
+    }
+}
+
+function readFromCache(fd, off, len, buf, cb) {
+    let file = cache[fd]
+    let rc = file.read
+    if (rc.block && off >= rc.off && off + len <= rc.off + rc.block.length) {
+        let boff = off - rc.off
+        rc.block.copy(buf, 0, boff, boff + len)
+        cb(len)
+    } else {
+        performP('read', (code, resultBuffer) => {
+            if (code >= 0 && resultBuffer) {
+                rc.off = off
+                rc.block = resultBuffer
+                let elen = Math.min(len, rc.block.length)
+                rc.block.copy(buf, 0, 0, elen)
+                cb(elen)
+            } else {
+                cb(code < 0 ? code : -70)
+            }
+        }, file.p, off, Math.max(len, blocksize))
+    }
+}
+
+function flushCacheFile(file, cb) {
+    let wc = file.write
+    if (wc.blocks.length > 0) {
+        let off = wc.off
+        wc.off = 0
+        wc.pos = 0
+        let buf = Buffer.concat(wc.blocks)
+        wc.blocks = []
+        performI('write', len => len < 0 ? cb(len) : cb(0), file.p, buf, off)
+    } else {
+        cb(0)
+    }
+}
+
+function flushCacheDescriptor(fd, cb) {
+    let file = cache[fd]
+    flushCacheFile(file, cb)
+}
+
+function writeToCache(fd, off, buf, cb) {
+    buf = Buffer.from(buf)
+    let file = cache[fd]
+    let wc = file.write
+    if (off == wc.pos && (wc.pos - wc.off + buf.length) <= blocksize) {
+        wc.blocks.push(buf)
+        wc.pos += buf.length
+        cb(buf.length)
+    } else {
+        flushCacheFile(file, code => {
+            if (buf.length > blocksize) {
+                performI('write', cb, file.p, buf, off)
+            } else {
+                wc.blocks.push(buf)
+                wc.off = off
+                wc.pos = off + buf.length
+                cb(buf.length)
+            }
+        })
+    }
+}
+
+function releaseCache(fd, cb) {
+    let file = cache[fd]
+    delete cache[fd]
+    flushCacheFile(file, cb)
+}
+
 fuse.mount(mountPath, {
-    getattr:  (p, cb)               => {
+    getattr: (p, cb) => {
         let cached = getAttrCache(p)
         if (cached) {
-            cb(0, cached)
+            cb(cached.code, cached.stat)
         } else {
             performP('getattr',  (code, stat) => { 
-                setAttrCache(p, stat)
+                setAttrCache(p, { code: code, stat: stat })
                 cb(code, stat)
             }, p)
         }
+    },
+    open: (p, flags, cb) => {
+        let fd = createDescriptor(p)
+        cb(0, fd)
+    },
+    create: (p, mode, cb) => {
+        performI('create', code => code < 0 ? cb(code) : cb(0, createDescriptor(p)), p, mode)
+    },
+    read: (p, fd, buf, len, off, cb) => {
+        if (isCached(fd)) {
+            readFromCache(fd, off, len, buf, cb)
+        } else {
+            performP('read', (code, resultBuffer) => {
+                if (code >= 0 && resultBuffer) {
+                    resultBuffer.copy(buf)
+                    cb(resultBuffer.length)
+                } else {
+                    cb(code < 0 ? code : -70)
+                }
+            }, p, off, len)
+        }
+    },
+    write: (p, fd, buf, len, off, cb) => {
+        let abuf = buf.length == len ? buf : buf.slice(0, len)
+        if (isCached(fd)) {
+            writeToCache(fd, off, abuf, cb)
+        } else {
+            performI('write', cb, p, abuf, off)
+        }
+    },
+    flush: (p, fd, cb) => {
+        isCached(fd) ? flushCacheDescriptor(fd, cb) : cb(0)
+    },
+    release: (p, fd, cb) => {
+        isCached(fd) ? releaseCache(fd, cb) : cb(0)
     },
     readdir:  (p, cb)                    => performP('readdir',  cb, p),
     truncate: (p, size, cb)              => performI('truncate', cb, p, size),
     readlink: (p, cb)                    => performP('readlink', cb, p),
     chown:    (p, uid, gid, cb)          => performI('chown',    cb, p, uid, gid),
     chmod:    (p, mode, cb)              => performI('chmod',    cb, p, mode),
-    read:     (p, fd, buf, len, off, cb) => {
-        performP('read', (code, resultBuffer) => {
-            if (code >= 0 && resultBuffer) {
-                resultBuffer.copy(buf)
-                cb(resultBuffer.length)
-            } else {
-                cb(code < 0 ? code : -70)
-            }
-        }, p, off, len)
-    },
-    write:    (p, fd, buf, len, off, cb) => {
-        performI('write', cb, p, buf.length == len ? buf : buf.slice(0, len), off)
-    },
-    create:   (p, mode, cb)              => performI('create',   cb, p, mode),
     utimens:  (p, atime, mtime, cb)      => performI('utimens',  cb, p, atime, mtime),
     unlink:   (p, cb)                    => performI('unlink',   cb, p),
     rename:   (p, dest, cb)              => performI('rename',   cb, p, dest),
